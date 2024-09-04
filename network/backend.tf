819b2897c92980fe7ba89da6ef3f7530eef2ac81
mathanprasathcogniwide <mathanprasath.kalaiventhan@cogniwide.com>
2024-09-04 10:00:06 +0530

    Delete network directory

--- network/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-state-file-mathanprasath-cogniwide"
-    key            = "terraform/vpc/vpc.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "DynamoDB_terraform_state_lock"
-  }
-}
