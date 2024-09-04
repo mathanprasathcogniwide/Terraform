c2d440107c9fc98d42be1f3b361899adf9b6d095
mathanprasathcogniwide <mathanprasath.kalaiventhan@cogniwide.com>
2024-09-04 09:59:35 +0530

    Delete ec2 directory

--- ec2/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-state-file-mathanprasath-cogniwide"
-    key            = "terraform/ec2/ec2.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "DynamoDB_terraform_state_lock"
-
-  }
-}
