e1b763a15f36ae1ffe18f5613476c7c7c179640f
mathanprasathcogniwide <mathanprasath.kalaiventhan@cogniwide.com>
2024-09-04 09:59:20 +0530

    Delete db directory

--- db/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-state-file-mathanprasath-cogniwide"
-    key            = "terraform/db/db.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "DynamoDB_terraform_state_lock"
-  }
-}
--- db/data.tf
+++
@@ -1,9 +0,0 @@
-data "terraform_remote_state" "vpc" {
-  backend = "s3"
-
-  config = {
-    bucket = "terraform-state-file-mathanprasath-cogniwide"
-    key    = "terraform/vpc/vpc.tfstate"
-    region = "us-east-1"
-  }
-}
